17639c4b2f400d81aa05368042d88300586a77f8
Batuhan Tekmen <44123432+googleaks-btTekmen@users.noreply.github.com>
2019-07-23 00:03:49 +0300

    Bence güzel kod :D

--- tree.py
+++ tree.py
@@ -1,22 +1,27 @@
+#Binary treelerde inorder, postorder ve preorder sıralanışlarını yazdıran fonksiyonlar modülü.
+
+#Tree'nin her nodunu "Node object" olarak kaydederek treeyi aklımda tutuyorum adjacency list şeklinde.
 class Node:
-    #a = 10
     def __init__(self, key):
-        #self.a = "instance attribute" -- bu ne ben de bilmiyorum :D
+        #self.a = "instance attribute" -- bu ne ben de bilmiyorum o yüzden yoruma attım boş bir şey herhalde.:D
         self.left = None
         self.right = None
         self.val = key
+        
 
 def inorder(root):
     if root:
-        ınorder(root.left) # indorder(7) > indorder(3) > inorder(2) > inorder(?)
+        ınorder(root.left)
         print(root.val)
         inorder(root.right)
+        
 
 def postorder(root):
     if root:
         postorder(root.left)
         postorder(root.right)
         print(root.val)
+        
 
 def preorder(root):
     if root:
