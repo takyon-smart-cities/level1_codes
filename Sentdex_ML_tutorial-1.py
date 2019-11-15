2db8776ce101afb0d5f4c535f68125a6cbf49ec2
Batuhan Tekmen <44123432+googleaks-btTekmen@users.noreply.github.com>
2019-11-15 22:21:16 +0300

    Create Sentdex_ML_tutorial-1.py

---
+++ Sentdex_ML_tutorial-1.py
@@ -0,0 +1,24 @@
+import numpy
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+import quandl
+
+df = quandl.get("WIKI/GOOGL")
+
+"""
+#Just to try syntax of pandas
+#for column in df.columns:
+#    print(column)
+#print(df[["Adj. Open",]])
+#print(df)
+"""
+
+
+#data wraggling for logistic regression
+df = df[["Adj. Open", "Adj. High","Adj. Low","Adj. Close","Adj. Volume"]]
+df["HL_PCT"] = df["Adj. High"] - df["Adj. Close"] / df["Adj. Close"] * 100
+df["PCT_change"] = df ["Adj. Close"] - df["Adj. Open"] / df["Adj. Open"] * 100
+
+df = df[["Adj. Close", "PCT_change", "HL_PCT", "Adj. Volume"]]
+#print(df)
